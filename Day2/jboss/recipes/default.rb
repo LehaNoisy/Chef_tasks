2e16ff752eda8116cfa22a14c306e57cbecc48e8
aliaksandrzaitsau <aliaksandr_zaitsau@epam.com>
2018-05-08 23:00:56 +0300

    chef task

---
+++ Day2/jboss/recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook:: jboss
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+#
+
+include_recipe 'java'
+package 'unzip'
+
+# Jboss user
+user 'jboss' do
+  # User for jboss service'
+  uid '1212'
+  shell '/bin/bash'
+end
+
+# Downloading jboss
+# source 'http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip' 
+remote_file './jboss.zip' do
+  source 'https://kent.dl.sourceforge.net/project/jboss/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA.zip'
+  show_progress true
+end
+
+# Unzip Jboss.zip
+bash 'unarchive' do
+  code <<-EOH
+    mkdir -p /opt/jboss
+    unzip jboss.zip -d /opt
+    cp -r /opt/jboss-5.1.0.GA/* /opt/jboss/
+    chown -R jboss:jboss /opt/jboss
+    EOH
+end
+
+# Jboss systemd
+template '/etc/systemd/system/jboss.service' do
+  source 'jboss.service.erb'
+  mode '0744'
+end
+
+# Jboss service
+service 'jboss' do
+  action [:enable, :start]
+end
+
+remote_file '/opt/jboss/server/default/deploy/sample.war' do
+  source 'https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war'
+end
